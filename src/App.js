c12d707225dfa001389b0293a545e68466412ccd
AkhilaavvaruPDS <akhilaavvaru@Privacydatasystems.com>
2023-11-20 20:26:06 +0530

    starter code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
